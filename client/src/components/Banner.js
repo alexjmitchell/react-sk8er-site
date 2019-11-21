9cdf82452f1c2ff5807feb3b6ad8f2dc77a6f7ca
Alex Mitchell <alexj.mitchell94@gmail.com>
2019-11-21 14:07:12 -0800

    got banner up and finished

--- client/src/components/Banner.js
+++ client/src/components/Banner.js
@@ -2,7 +2,14 @@ import React from "react"
 import "../styles/Banner.css"
 
 const Banner = props => {
-  return <div className="banner"></div>
+  return (
+    <section className="banner-container">
+      <div className="banner"></div>
+      <div className="overlay"></div>
+      <div className="logo">
+      </div>
+    </section>
+  )
 }
 
 export default Banner
